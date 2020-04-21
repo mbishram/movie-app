48e68a518265dd0f4beae9c423a8aab13db9b832
Muh Bisram Yashir A A <m.bishram.yashir@gmail.com>
2020-04-21 13:59:26 +0800

    Added core js to move between items

---
+++ src/data/clothes.js
@@ -0,0 +1,44 @@
+const clothes = [
+    {
+        type: "analogic",
+        color: "blue",
+        name: "Analogic Shirt Blue Langit",
+        price: "100.000",
+        image: "./src/images/clothes/analogic-skyblue.png"
+    },
+    {
+        type: "analogic",
+        color: "white",
+        name: "Analogic Shirt White",
+        price: "100.000",
+        image: "./src/images/clothes/analogic-white.png"
+    },
+    {
+        type: "koftte",
+        color: "black",
+        name: "KOFTTE Shirt Black",
+        price: "98.000",
+        image: "./src/images/clothes/kofte-front.png"
+    },
+    {
+        type: "sarcastic",
+        color: "blue",
+        name: "Sarcastic? Shirt Biru Navy",
+        price: "95.000",
+        image: "./src/images/clothes/satire-baju-bluenavy.png"
+    },
+    {
+        type: "sarcastic",
+        color: "gray",
+        name: "Sarcastic? Shirt Abu2",
+        price: "95.000",
+        image: "./src/images/clothes/satire-baju-abu2.png"
+    },
+    {
+        type: "sarcastic",
+        color: "black",
+        name: "Sarcastic? Shirt Hitam",
+        price: "95.000",
+        image: "./src/images/clothes/satire-baju-black.png"
+    }
+]
--- src/ui/main.js
+++ src/ui/main.js
@@ -1,3 +1,93 @@
-const openNav = () => document.querySelector("#cart").style.right = "0";
+// Variable declaration
+let itemID = "analogic";
+let colorID = document.querySelector(`#variant-${itemID} .selected`).id;
 
+// Get list of ItemIDs
+const itemLists = toArray(document.querySelector("#list-items").children);
+const itemListsID = itemLists.map(item => item.id);
+
+// Setup the page!
+setListItems(itemID);
+getSelectedItem(colorID);
+
+// Set the selected ItemID
+function setItemID(itemId) {
+    console.log(itemID, colorID)
+    itemID = itemId;
+    setListItems(itemId);
+}
+
+// Set the view using selected ItemID
+function setListItems(selectedItem) {
+    for (const item of itemListsID) {
+        const itemElement = document.querySelector(`#${item}`);
+        if (item == selectedItem) {
+            itemElement.classList.add("selected");
+        } else {
+            itemElement.classList.remove("selected");
+        }
+    }
+    console.log(document.querySelector(`#variant-${itemID}>*:first-child`).id)
+    setColorID(document.querySelector(`#variant-${itemID}>*:first-child`).id)
+
+}
+
+// Set the selected ColorID
+function setColorID(colorId) {
+    console.log(itemID, colorID)
+    colorID = colorId;
+    getSelectedItem(colorId);
+}
+
+// Get the data from clothes.js
+function getSelectedItem(colorId) {
+    const query = colorId.split("-");
+    const itemData = clothes.filter(item => item.type.toLowerCase().includes(query[0].toLowerCase()) && item.color.toLowerCase().includes(query[1].toLowerCase()));
+    setSelectedItem(...itemData);
+}
+
+// Set the selected Item
+function setSelectedItem(itemData) {
+    const itemId = `${itemData.type}-${itemData.color}`;
+    const variantId = `variant-${itemData.type}`;
+    const selectedVariantChildren = toArray(document.querySelector(`#${variantId}`).children);
+    const selectedVariantChildrenId = selectedVariantChildren.map(item => item.id);
+    for (const variant of selectedVariantChildrenId) {
+        const variantElement = document.querySelector(`#${variant}`);
+        if (variant == itemId) {
+            variantElement.classList.add("selected");
+        } else {
+            variantElement.classList.remove("selected");
+        }
+    }
+
+    document.querySelector("#item-name").textContent = itemData.name;
+    document.querySelector("#item-price").textContent = itemData.price;
+    document.querySelector("#item-image").src = itemData.image;
+
+    const variantChildren = toArray(document.querySelector("#item-variant").children);
+    const variantChildrenId = variantChildren.map(item => item.id);
+    for (const variant of variantChildrenId) {
+        const variantElement = document.querySelector(`#${variant}`);
+        if (variant != variantId) {
+            variantElement.classList.add("hidden");
+        } else {
+            variantElement.classList.remove("hidden");
+        }
+    }
+
+    document.querySelector("#quantity").value = 1
+}
+
+function toArray(collection) {
+    const colArray = [];
+    for (const col of collection) {
+        colArray.push(col);
+    }
+
+    return colArray;
+}
+
+// Control the opening and closing of cart
+const openNav = () => document.querySelector("#cart").style.right = "0";
 const closeNav = () => document.querySelector("#cart").style.right = "-22rem";
