1552f482d8ed34863d740c705298a6c137d3f17e
Muh Bisram Yashir A A <m.bishram.yashir@gmail.com>
2020-05-06 22:33:57 +0800

    Fixing images won't show up on /dist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import "regenerator-runtime";
 
 import "./styles/style.scss";
 
+import "./images/moview-app-logo.png";
+import "./images/construction.svg";
+import "./images/how-to-train-your-dragon.jpg";
+
 import main from "./script/ui/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
--- webpack.common.js
+++ webpack.common.js
@@ -2,42 +2,57 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scs|sas|cs)s$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "sass-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            filename: "index.html",
-            favicon: "./src/images/logo.png"
-        })
-    ]
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scs|sas|cs)s$/,
+				use: [
+					{
+						loader: "style-loader",
+					},
+					{
+						loader: "css-loader",
+					},
+					{
+						loader: "sass-loader",
+					},
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							outputPath: "font/",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(gif|jpg|png|svg)$/i,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "images/",
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			filename: "index.html",
+			favicon: "./src/images/logo.png",
+		}),
+	],
 };
